c04924c1dc989d5d67862312f9bfc1cec6252ea7
Useles5 <126971764+Useles5@users.noreply.github.com>
2026-08-09 09:56:35 +0530

    feat(auth): implement login POST request and session cookie capture
    - Added a cookiejar to the custom http client to cpature JSESSIONID.
    
    - Implemented exact POST request to /cgi/login, bypassing Go's url.Values alphabetical sorting to preserve the exact order expected by the router

--- pkg/archerC5/auth.go
+++ pkg/archerC5/auth.go
@@ -5,12 +5,21 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/http/cookiejar"
+	"net/url"
 	"regexp"
 )
 
 func Authenticate(password string) error {
+
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		return fmt.Errorf("failed to create cookie jar: %v", err)
+	}
 	// Custom client
-	client := &http.Client{}
+	client := &http.Client{
+		Jar: jar,
+	}
 
 	req, err := http.NewRequest(http.MethodGet, "http://tplinkwifi.net/cgi/getParm", nil)
 	if err != nil {
@@ -52,11 +61,70 @@ func Authenticate(password string) error {
 	exponentHex := eeMatch[1]
 	modulusHex := nnMatch[1]
 
-	encryptedPayload, err := EncryptPayload(password, modulusHex, exponentHex)
+	encryptedUsername, err := EncryptPayload("admin", modulusHex, exponentHex)
+	if err != nil {
+		return fmt.Errorf("failed to encrypt username: %w", err)
+	}
+	encryptedPassword, err := EncryptPayload(password, modulusHex, exponentHex)
 	if err != nil {
 		return fmt.Errorf("failed to encrypt password: %w", err)
 	}
-	fmt.Printf("Success! Encrypted Payload generated: %s...\n", encryptedPayload[:30])
+
+	// Construct the payload
+	// url.Values automatically handles url encoding
+
+	// Had to abandon because url.Values automatically sorts keys in alphabetical order
+	// Router needs the queryString in correct Order unlike modern web servers
+
+	//formData := url.Values{}
+	//formData.Set("UserName", encryptedUsername)
+	//formData.Set("Passwd", encryptedPassword)
+	//formData.Set("Action", "1")
+	//formData.Set("LoginStatus", "0")
+
+	//queryString := formData.Encode()
+
+	queryString := fmt.Sprintf("UserName=%s&Passwd=%s&Action=1&LoginStatus=0",
+		url.QueryEscape(encryptedUsername),
+		url.QueryEscape(encryptedPassword),
+	)
+	fmt.Println(queryString)
+	fullURL := fmt.Sprintf("http://tplinkwifi.net/cgi/login?%s", queryString)
+
+	// Data is in URL, no body is needed so nil
+	loginReq, err := http.NewRequest(http.MethodPost, fullURL, nil)
+	if err != nil {
+		return fmt.Errorf("failed to create request: %w", err)
+	}
+
+	// Set Headers
+	loginReq.Header.Set("Origin", "http://tplinkwifi.net")
+	loginReq.Header.Set("Referer", "http://tplinkwifi.net/")
+	loginReq.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36")
+	loginReq.Header.Set("Accept", "*/*")
+
+	loginRes, err := client.Do(loginReq)
+	if err != nil {
+		return fmt.Errorf("failed to send request: %w", err)
+	}
+	defer loginRes.Body.Close()
+
+	if loginRes.StatusCode != http.StatusOK {
+		return fmt.Errorf("login failed with status %d", loginRes.StatusCode)
+	}
+
+	loginBodyBytes, err := io.ReadAll(loginRes.Body)
+	if err != nil {
+		return fmt.Errorf("failed to read response body: %w", err)
+	}
+
+	fmt.Printf("Login Response: %s\n", string(loginBodyBytes))
+
+	// Set Cookie
+	loginURL, _ := url.Parse("http://tplinkwifi.net")
+	cookies := client.Jar.Cookies(loginURL)
+	fmt.Printf("Captured Session Cookies: %v\n", cookies)
+
 	return nil
 
 }
