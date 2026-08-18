bb3512208927e33f7fdf0dfe9e3a0aa358eec20f
Useles5 <126971764+Useles5@users.noreply.github.com>
2026-08-18 12:14:39 +0530

    fix: update middleware to use app.writeJSON instead of http.Error for consistency with the JSON response contract

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -8,10 +8,10 @@ import (
 
 func (app *application) requireAPIKey(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		providedKey := r.Header.Get("X-API-KEY")
+		providedKey := r.Header.Get("X-API-Key")
 		if providedKey == "" {
-			msg := fmt.Sprintf("%s: Missing X-API-Key  in header", http.StatusText(http.StatusUnauthorized))
-			http.Error(w, msg, http.StatusUnauthorized)
+			msg := fmt.Sprintf("%s: Missing X-API-Key in header", http.StatusText(http.StatusUnauthorized))
+			app.writeJSON(w, http.StatusUnauthorized, nil, msg)
 			return
 		}
 
@@ -19,7 +19,7 @@ func (app *application) requireAPIKey(next http.HandlerFunc) http.HandlerFunc {
 		// use subtle.ConstantTimeCompare() to prevent timing attacks
 		if subtle.ConstantTimeCompare([]byte(providedKey), []byte(app.config.apiKey)) != 1 {
 			msg := fmt.Sprintf("%s: Invalid API Key", http.StatusText(http.StatusUnauthorized))
-			http.Error(w, msg, http.StatusUnauthorized)
+			app.writeJSON(w, http.StatusUnauthorized, nil, msg)
 			return
 		}
 
